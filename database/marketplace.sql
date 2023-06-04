1ef32d383104357ab3fc45789c7611412cb3d0ca
siplancelot <sip.lancelot@gmail.com>
2023-06-04 14:02:29 +0700

    upload foto and update database

--- database/marketplace.sql
+++ database/marketplace.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Jun 2023 pada 04.16
+-- Waktu pembuatan: 04 Jun 2023 pada 09.01
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.1.12
 
@@ -31,16 +31,39 @@ CREATE TABLE `petugas` (
   `id_petugas` int(11) NOT NULL,
   `nama_petugas` varchar(255) NOT NULL,
   `telp_petugas` varchar(15) NOT NULL,
-  `email_petugas` varchar(100) NOT NULL
+  `email_petugas` varchar(100) NOT NULL,
+  `foto_petugas` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `petugas`
 --
 
-INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `telp_petugas`, `email_petugas`) VALUES
-(1, 'Agus Mahardika', '084342424242', 'agus_mahardika@gmail.com'),
-(2, 'Valensyah', '089513252231', 'valen@gmail.com');
+INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `telp_petugas`, `email_petugas`, `foto_petugas`) VALUES
+(1, 'Agus Mahardika', '081231414343', 'agus_mahardika@gmail.com', 'foto.png'),
+(3, 'tqwfqwvwvwvw', '2141414', 'vewvwvew@gmail.com', 'Samsung-Z-Flip1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `email`, `password`) VALUES
+(1, 'aldi', 'aldi@gmail.com', '25d55ad283aa400af464c76d713c07ad'),
+(2, 'safasfafa', 'aldimrahim@gmail.com', '25d55ad283aa400af464c76d713c07ad'),
+(3, 'esdgvdsvsbvs', 'aldim@gmail.com', '25d55ad283aa400af464c76d713c07ad');
 
 --
 -- Indexes for dumped tables
@@ -53,6 +76,12 @@ ALTER TABLE `petugas`
   ADD PRIMARY KEY (`id_petugas`);
 
 --
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -60,7 +89,13 @@ ALTER TABLE `petugas`
 -- AUTO_INCREMENT untuk tabel `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
